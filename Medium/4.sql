8872ebe9ab81335c82d6c9066214f999a691a91c
Aashna J Chandrashekhar <aashnajc@gmail.com>
2024-07-06 11:15:43 +0530

    4.sql

---
+++ Medium/4.sql
@@ -0,0 +1,13 @@
+/*Show all columns for patient_id 542's most recent admission_date.*/
+
+select * from admissions
+where patient_id = 542
+group by patient_id
+having admission_date = max(admission_date);
+
+select * from admissions
+where patient_id = 542
+and 
+admission_date = 
+( select max(admission_date) from admissions 
+ where patient_id = 542);
